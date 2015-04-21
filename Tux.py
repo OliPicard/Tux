f4b2e3a11dde2c82fb978fcb1a3f9669ef31b5d8
Oliver Bryant <oliver@ufomail.co.uk>
2015-04-21 15:28:57 +0000

    added duck duck go support because emacs.

--- Tux.py
+++ Tux.py
@@ -24,7 +24,22 @@ def menu():
     print('Welcome to Tux, Your friendly, intelligent AI.')
     print('This program is licenced under the GPL GNU 3.0 licence.')
 
-
+def duckduckgo():
+    words = input('Ok, So how can I be of assistance? ')
+    if words == 'quit':
+        sys.exit()
+    elif words == 'credits':
+        print('Tux was created by OliPicard. full source code at github.com/olipicard/tux')
+        print('This program is licenced under the GPL GNU 3.0 licence.')
+        print('Original idea from /r/linux/')
+        print('Tux is the official mascot of Linux.')
+    elif words == 'help':
+        print('Simply ask me any question. to quit the application type quit.')
+    else:
+        url = 'http://duckduckgo.com/?q='
+        output = re.sub('\s', '+', words.rstrip())
+        print('Ok, Here\'s what i\'ve found\n'+url + output)
+        input('Press any key to return back to the main menu.')
 
 def lmgtfy():
     words = input('Ok, So how can I be of assistance? ')
